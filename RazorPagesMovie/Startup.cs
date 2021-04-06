6ac365b2a469e1ba99ecafa6ad2d608da6421c37
Emmanuel Lemaistre <manu9576@hotmail.fr>
2021-04-06 20:05:38 +0200

    Validation with localization corrected

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -58,6 +58,15 @@ namespace RazorPagesMovie
             {
                 endpoints.MapRazorPages();
             });
+
+            var defaultCulture = new CultureInfo("fr-FR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationOptions);
         }
     }
 }
