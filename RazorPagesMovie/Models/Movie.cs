a989124648a10ecf211ab31976284bda406906f5
Emmanuel Lemaistre <manu9576@hotmail.fr>
2021-04-10 16:02:12 +0200

    Add new data constrain

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -8,17 +8,29 @@ namespace RazorPagesMovie.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60,MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Range(typeof(DateTime), "1/1/1966", "1/1/2020")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName= "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
