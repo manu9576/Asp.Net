8e755c5d4810fb910cb36803213df7f427a05ba9
Emmanuel Lemaistre <manu9576@hotmail.fr>
2021-04-12 20:05:20 +0200

    Creation of MVC tuto

---
+++ AspNetMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace AspNetMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /helloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /helloWorld/Welcome
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+
+    }
+}
