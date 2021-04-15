7d2bbb3bfb6fed5281ea8e8db7e9cf1d583359bb
Emmanuel Lemaistre <manu9576@hotmail.fr>
2021-04-15 22:27:06 +0200

    Correction of Movie model
    Add missing annotation for price

--- AspNetMVC/Models/Movie.cs
+++ AspNetMVC/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AspNetMVC.Models
 {
@@ -11,6 +12,8 @@ namespace AspNetMVC.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
